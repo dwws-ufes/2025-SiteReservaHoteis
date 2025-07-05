bdd02fcecba671384a0bbe8196269b7d17d5b118
Norman Frieman <ezequiel-d.s@hotmail.com>
2025-07-05 12:09:22 -0300

    fix: add sln project

--- Parte2/ReservaHotelBack/ReservaHotel/Program.cs
+++ Parte2/ReservaHotelBack/ReservaHotel/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4201")
+            policy.WithOrigins("http://localhost:4200", "http://localhost:4201")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
