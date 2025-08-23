528cc0ed698d4df0ef2b05c5596fdc123c837f3f
Erico Goncalves Guedes <erico.guedes@edu.ufes.br>
2025-08-23 15:55:13 -0300

    Maps Working

--- Parte2/ReservaHotelFront/src/environments/environment.ts
+++ Parte2/ReservaHotelFront/src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: false,
+  googleMapsApiKey: 'AIzaSyC-8AhBDxCpMMdD--d0NRRq-gIdwa5BCTM',
   // url: 'http://localhost:5058',
   url: 'https://localhost:7099'
 };
