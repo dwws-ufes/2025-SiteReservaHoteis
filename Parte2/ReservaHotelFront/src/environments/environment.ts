987ddb30aee08b8698f80e967d49287d517cde44
Erico Goncalves Guedes <erico.guedes@edu.ufes.br>
2025-08-29 13:13:34 -0300

    Retirada da key pessoal da API

--- Parte2/ReservaHotelFront/src/environments/environment.ts
+++ Parte2/ReservaHotelFront/src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  googleMapsApiKey: 'AIzaSyC-8AhBDxCpMMdD--d0NRRq-gIdwa5BCTM',
+  googleMapsApiKey: '',
   url: 'http://localhost:5058',
   //url: 'https://localhost:7099'
 };
